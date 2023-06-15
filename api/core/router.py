8abffb19d5ccda7362d17e88c3f6bee31395a607
Ethan Saxenian <ethansaxenian+github@proton.me>
2023-06-14 20:18:34 -0400

    remove /stats endpoint

--- api/core/router.py
+++ api/core/router.py
@@ -2,11 +2,9 @@ from fastapi import APIRouter
 
 from months.routes import months_router
 from plans.routes import plans_router
-from stats.routes import stats_router
 from transactions.routes import transactions_router
 
 api_router = APIRouter()
 api_router.include_router(transactions_router)
 api_router.include_router(plans_router)
 api_router.include_router(months_router)
-api_router.include_router(stats_router)
