cbf23b09a49a5155ae10db1c523944c45689fbd9
Ethan Saxenian <ethansaxenian+github@proton.me>
2025-01-15 21:08:46 -0500

    fixes

--- util/types.go
+++ util/types.go
@@ -5,23 +5,23 @@ import "github.com/ethansaxenian/budgeting/database"
 var EXPENSE_CATEGORIES = []database.Category{
 	database.CategoryFood,
 	database.CategoryGifts,
-	database.CategoryMedical,
 	database.CategoryHome,
-	database.CategoryTransportation,
+	database.CategoryMedical,
+	database.CategoryOther,
 	database.CategoryPersonal,
 	database.CategorySavings,
-	database.CategoryUtilities,
+	database.CategoryTransportation,
 	database.CategoryTravel,
-	database.CategoryOther,
+	database.CategoryUtilities,
 }
 
 var INCOME_CATEGORIES = []database.Category{
-	database.CategoryPaycheck,
 	database.CategoryBonus,
-	database.CategoryInterest,
-	database.CategoryGifts,
-	database.CategoryOther,
 	database.CategoryCashback,
+	database.CategoryGifts,
+	database.CategoryInterest,
+	database.CategoryOther,
+	database.CategoryPaycheck,
 }
 
 var CATEGORIES_BY_TYPE = map[database.TransactionType][]database.Category{
